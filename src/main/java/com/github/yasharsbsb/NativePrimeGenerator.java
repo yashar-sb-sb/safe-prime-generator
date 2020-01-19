b4b68da20f0b98bb1e74029a18c771c0e31beef1
Yashar Shahi <yashar_sb_sb@yahoo.com>
2020-01-19 13:01:25 +0330

    Put lib in a random tmp file.

--- src/main/java/com/github/yasharsbsb/NativePrimeGenerator.java
+++ src/main/java/com/github/yasharsbsb/NativePrimeGenerator.java
@@ -1,5 +1,6 @@
 package com.github.yasharsbsb;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 
@@ -14,10 +15,10 @@ public class NativePrimeGenerator {
     private static boolean NATIVE_LIBRARY_LOADED = false;
 
     static synchronized void loadNativeLibrary () {
-        String lib = "/tmp/libnative.so";
         if (!NATIVE_LIBRARY_LOADED) {
             InputStream in = NativePrimeGenerator.class.getResourceAsStream ("/libnative.so");
             try {
+                File lib = File.createTempFile("lib-", ".so");
 
                 int read;
                 byte[] bytes = new byte[1024];
@@ -27,10 +28,10 @@ public class NativePrimeGenerator {
                     outputStream.write (bytes , 0 , read);
                 }
                 outputStream.close ();
-                System.load (lib);
+                System.load (lib.getAbsolutePath());
                 NATIVE_LIBRARY_LOADED = true;
             } catch (Exception e) {
-                System.out.println ("Faile to load file : " + lib);
+                System.out.println ("Faile to load native lib");
             }
         }
     }
